f92b6eff90e864c8d8d492f3ec868b2e55c4f3bf
Syrup <goodgamersz665@gmail.com>
2024-08-31 23:23:12 +0700

    refactor: Update cookies.server.ts to use environment variables for secrets

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -3,7 +3,7 @@ import { createCookie } from "@remix-run/node";
 const oneweek = 60 * 60 * 24 * 7; // 1 week
 
 export const login = createCookie("__login", {
-  secrets: ["S3CR3T"],
+  secrets: [process.env.LOGIN_SECRET!], // haha
   maxAge: oneweek,
 });
 
@@ -11,7 +11,7 @@ const sesionExpires = new Date();
 sesionExpires.setSeconds(sesionExpires.getSeconds() + oneweek);
 
 export const sessionCookie = createCookie("__session", {
-  secrets: [process.env.LOGIN_SECRET!], // haha i changed it lol
+  secrets: [process.env.SESSION_SECRET!], // haha i changed it lol
   sameSite: true,
   expires: sesionExpires,
 });
