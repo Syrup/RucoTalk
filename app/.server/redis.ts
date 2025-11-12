90fa56a76651d3fc45faf0b6faebd6aa71da8245
Fabian Maulana <goodgamersz665@gmail.com>
2025-11-12 04:11:08 +0000

    refactor: update Redis client configuration to use Upstash URL and remove deprecated socket settings

--- app/.server/redis.ts
+++ app/.server/redis.ts
@@ -1,11 +1,7 @@
 import { createClient } from "redis";
 
 const client = createClient({
-  password: "rww09binPrxgBfbJSWdP61vTJCYOnMea",
-  socket: {
-    host: "redis-16932.c85.us-east-1-2.ec2.redns.redis-cloud.com",
-    port: 16932,
-  },
+  url: "rediss://default:AR5kAAImcDIyYWEwMGRiNTI2YmM0YmU4YTBkMmYzYTY4YmVhMWNjMHAyNzc4MA@assured-moccasin-7780.upstash.io:6379"
 });
 
 client.on("error", (err) => console.log("Redis Client Error", err));
